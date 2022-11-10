af84c64eb5ba39652a6ab72912a8c857c4bbc534
junho <junholim98@gmail.com>
2022-11-10 21:34:01 +0900

    게임해듀오 홈화면 완성(내부로직 추가 개발 필요)

--- lib/page/login/startPage.dart
+++ lib/page/login/startPage.dart
@@ -1,8 +1,6 @@
-import 'package:cau_gameduo/components/Color.dart';
-import 'package:cau_gameduo/controller/login/SignUpController.dart';
-import 'package:cau_gameduo/page/login/kakao_login.dart';
+import 'package:cau_gameduo/page/login/kakao_loginV1.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../components/SizedBox.dart';
 
@@ -14,25 +12,98 @@ class StartPage extends StatefulWidget {
 }
 
 class _StartPageState extends State<StartPage> {
-  final kakoLogin = KakaoLogin();
+  final kakaoLogin = KakaoLogin();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            IconButton(
-              icon: Image.asset('images/kakao_login_medium_wide.png'),
-              iconSize: 300,
-              onPressed: () {
-                kakoLogin.login();
-              },
-            )
-          ]),
-        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Sbox(double.infinity, 0),
+              Image(
+                image: AssetImage('images/lol_icon.png'),
+                width: 139,
+                height: 150,
+              ),
+              Sbox(0, 10),
+              Text(
+                '게임해듀오',
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xFF545DAD),
+                    fontWeight: FontWeight.bold),
+              ),
+              Sbox(0, 280),
+              GestureDetector(
+                  child: Container(
+                      width: 300,
+                      height: 45,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(
+                          image:
+                              AssetImage("images/kakao_login_medium_wide.png"),
+                          fit: BoxFit.cover,
+                        ), // button text
+                      )),
+                  onTap: () {
+                    kakaoLogin.login();
+                  }),
+              Sbox(0, 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '로그인하면 게임해듀오 ',
+                    style: TextStyle(fontSize: 11),
+                  ),
+                  GestureDetector(
+                      child: Text("이용약관",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 11,
+                              fontWeight: FontWeight.bold)),
+                      onTap: () {
+                        launchUrl(
+                          Uri.parse('https://gamehaeduo.shop/policy'),
+                        );
+                      }),
+                  Text(
+                    '에 동의하는 것으로 간주됩니다.',
+                    style: TextStyle(fontSize: 11),
+                  ),
+                ],
+              ),
+              Sbox(0, 2),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '게임해듀오 회원정보 처리 방식은 ',
+                    style: TextStyle(fontSize: 11),
+                  ),
+                  GestureDetector(
+                      child: Text("개인정보취급방침",
+                          style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 11,
+                              fontWeight: FontWeight.bold)),
+                      onTap: () {
+                        launchUrl(
+                          Uri.parse('https://gamehaeduo.shop/privacy'),
+                        );
+                      }),
+                  Text(
+                    '에서 확인 가능합니다.',
+                    style: TextStyle(fontSize: 11),
+                  ),
+                ],
+              ),
+              Sbox(0, 40),
+            ]),
       ),
     );
   }
 }
-
-// mage : Image.asset("kakao_login_large_wide.png")
