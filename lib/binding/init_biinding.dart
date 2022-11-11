b286966221ffd281f9da6247dc52cb1048ba1c05
Soongguri <yunju990116@gmail.com>
2022-11-11 17:39:45 +0900

    마이페이지/설정(미완)

--- lib/binding/init_biinding.dart
+++ lib/binding/init_biinding.dart
@@ -2,20 +2,18 @@ import 'package:cau_gameduo/controller/home/homePageController.dart';
 import 'package:cau_gameduo/controller/login/SignUpController.dart';
 import 'package:cau_gameduo/controller/becomePlayer/BecomePlayerController1.dart';
 import 'package:cau_gameduo/controller/becomePlayer/BecomePlayerController2.dart';
-import 'package:cau_gameduo/controller/myPage/profileController.dart';
-
+import 'package:cau_gameduo/controller/myPage/myPageController.dart';
+import 'package:cau_gameduo/controller/myPage/settingController.dart';
 import 'package:get/get.dart';
-
-import '../controller/bottomNavController.dart';
 
 class InitBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(SignUpController(), permanent: true);
-    Get.put(BottomNavController(), permanent: true);
     Get.put(BecomePlayerController1(), permanent: true);
     Get.put(BecomePlayerController2(), permanent: true);
     Get.put(homePageController(), permanent: true);
-    Get.put(profileController(), permanent: true);
+    Get.put(MyPageController(), permanent: true);
+    Get.put(SettingController(), permanent: true);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:cau_gameduo/components/Color.dart';
 import 'package:cau_gameduo/page/becomePlayer/becomePlayerPage1.dart';
 import 'package:cau_gameduo/page/becomePlayer/becomePlayerPage2.dart';
 import 'package:cau_gameduo/page/home/homePage.dart';
+import 'package:cau_gameduo/page/myPage/myPage.dart';
+import 'package:cau_gameduo/page/myPage/settingPage.dart';
 import 'package:cau_gameduo/page/login/signUpPage.dart';
 import 'package:cau_gameduo/page/login/startPage.dart';
-import 'package:cau_gameduo/page/myPage/profilePage.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
@@ -33,14 +34,15 @@ class MyApp extends StatelessWidget {
         ),
       ),
       initialBinding: InitBinding(),
-      home: const StartPage(),
+      home: const SettingPage(),
       getPages: [
         GetPage(name: '/start', page: () => StartPage()),
         GetPage(name: '/signup', page: () => SignUpPage()),
         GetPage(name: '/home', page: () => homePage()),
         GetPage(name: '/becomeplayer1', page: () => BecomePlayerPage1()),
         GetPage(name: '/becomeplayer2', page: () => BecomePlayerPage2()),
-        GetPage(name: '/profile', page: () => profilePage()),
+        GetPage(name: '/mypage', page: () => MyPage()),
+        GetPage(name: '/mypage', page: () => SettingPage()),
       ],
     );
   }
