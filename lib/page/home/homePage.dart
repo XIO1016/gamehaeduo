83591b0dbffc6e2b686d54e045e684f5be6e5d68
thddh <dhthdrud@naver.com>
2022-11-20 21:20:26 +0900

    fix: fix homePage listview error

--- lib/page/home/homePage.dart
+++ lib/page/home/homePage.dart
@@ -1,160 +1,199 @@
 import 'package:cau_gameduo/components/Color.dart';
-import 'package:cau_gameduo/page/becomePlayer/becomePlayerPage1.dart';
-import 'package:cau_gameduo/page/myPage/profilePage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:cau_gameduo/controller/home/homePageController.dart';
 import '../../components/SizedBox.dart';
-import 'package:cau_gameduo/page/myPage/settingPage.dart';
-import 'dart:math' as math;
+
 
 class homePage extends GetView<homePageController> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Obx(
-          () => Scaffold(
-            appBar: AppBar(
-              title:
-              Row(
-                children: [
-                  Image.asset(
-                    "images/logo.png",
-                    width: 50,
-                    height: 50,
-                  ),
-                  Text('게임해듀오',
-                      style: TextStyle(color: maincolor, fontSize: 14,) ),
-                ],
-              )),
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+    return Scaffold(
+      appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: Row(
             children: [
-              Text('별점이 높은 유저를 만나보세요',
-                  style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
-              Text('같이 게임을 즐길 듀오를 구해보세요',
-                  style: TextStyle(fontSize: 12,)),
-              Sbox(0, 10),
-              Container(
-                child: SingleChildScrollView(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      SizedBox(
-                        height: (controller.closeTopContainer.value==0)?150:180,
-                        child: ListView.builder(
-                          itemCount: 10,
-                          scrollDirection: Axis.horizontal,
-                          itemBuilder: (context, index) => Container(
-                            height: 180,
-                            width: 120,
-                            margin: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                                color: Color(0xFFD1D1D1),
-                                borderRadius: BorderRadius.circular(10),
-                            border: Border.all(
-                              width:1,
-                              color: Colors.grey,
-                            )),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Image.network(//프로필 사진
-                                  controller.myprofile.image,
-                                  fit: BoxFit.fitWidth,
-                                ),
-                                Sbox(0, 5),
-                                Text((controller.myprofile.nick=='')?'닉네임':controller.myprofile.nick),
-                                Sbox(0, 5),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                  Image.asset(//랭크 사진
-                                  "images/point.png",
-                                  width: 20,
-                                    height: 20,
-                                ),
-                                    Sbox(5, 0),
-                                    Text((controller.myprofile.tier=='')?'티어':controller.myprofile.tier),
-                                  ]
-                                ),
-                                Sbox(0, 5),
-                                Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      //Text(controller.myprofile.point,),
-                                      Sbox(5, 0),
-                                      Image.asset(
-                                        "images/point.png",
-                                        width: 20,
-                                        height: 20,
-                                      ),
-                                    ]
-                                )
-                              ],
+              Image.asset(
+                "images/logo.png",
+                width: 50,
+                height: 50,
+              ),
+              Sbox(10, 0),
+              Text('게임해듀오',
+                  style: TextStyle(
+                    color: maincolor,
+                    fontSize: 14,
+                  )),
+            ],
+          )),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: ScrollConfiguration(
+          behavior: const ScrollBehavior().copyWith(overscroll: false),
+          child: ListView(children: [
+            const Text('별점이 높은 유저를 만나보세요',
+                style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold)),
+            Sbox(0, 5),
+            const Text('같이 게임을 즐길 듀오를 구해보세요',
+                style: TextStyle(
+                  fontSize: 12,
+                )),
+            Sbox(0, 10),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              physics: const BouncingScrollPhysics(),
+              child: Row(
+                children: List.generate(
+                  10,
+                  (index) => Container(
+                    height: 193,
+                    width: 120,
+                    margin: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        color: const Color(0xFFF6F6F6),
+                        borderRadius: BorderRadius.circular(10),
+                        border: Border.all(
+                          width: 1,
+                          color: const Color(0xffD1D1D1),
+                        )),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius:
+                              BorderRadius.circular(20), // Image border
+                          child: SizedBox.fromSize(
+                            size: Size.fromRadius(60),
+                            child: Image.network(
+                              //프로필 사진
+                              controller.myprofile.image,
                             ),
                           ),
                         ),
-                      ),
-                      // Flexible(
-                      //   child: ListView.builder(itemCount: 10,
-                      //     scrollDirection: Axis.vertical,
-                      //     itemBuilder: (context, index) => Container(
-                      //       width: double.infinity,
-                      //       height: 90,
-                      //       margin: EdgeInsets.all(10),
-                      //       decoration: BoxDecoration(
-                      //           color: Color(0xFFD1D1D1),
-                      //           borderRadius: BorderRadius.circular(10),
-                      //           border: Border.all(
-                      //             width:1,
-                      //             color: Colors.grey,
-                      //           )),
-                      //       child: Row(
-                      //         mainAxisAlignment: MainAxisAlignment.center,
-                      //         children: [
-                      //           Image.network(//프로필 사진
-                      //             controller.myprofile.image,
-                      //             fit: BoxFit.fitHeight,
-                      //           ),
-                      //           Column(
-                      //               mainAxisAlignment: MainAxisAlignment.start,
-                      //               children: [
-                      //                 Text(controller.myprofile.nick),
-                      //                 Row(
-                      //                   children: [
-                      //                     Image.asset(//랭크 사진
-                      //                       "images/point.png",
-                      //                       width: 20,
-                      //                       height: 20,
-                      //                     ),
-                      //                     Text(controller.myprofile.tier),
-                      //                     //Text(controller.myprofile.point,),
-                      //                     Image.asset(
-                      //                       "images/point.png",
-                      //                       width: 20,
-                      //                       height: 20,
-                      //                     ),
-                      //                   ],
-                      //                 ),
-                      //               ]
-                      //           ),
-                      //         ],
-                      //       ),
-                      //     ),
-                      //   )
-                      // )
-                    ],
+                        Sbox(0, 5),
+                        Text(
+                          controller.myprofile.nick,
+                          style: const TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                        Sbox(0, 5),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image.asset(
+                                //랭크 사진
+                                "images/point.png",
+                                width: 18,
+                                height: 18,
+                              ),
+                              Sbox(5, 0),
+                              Text(
+                                '티어',
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.bold),
+                              ),
+                            ]),
+                        Sbox(0, 5),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              //Text(controller.myprofile.point,),
+                              Sbox(5, 0),
+                              Image.asset(
+                                "images/point.png",
+                                width: 18,
+                                height: 18,
+                              ),
+                            ]),
+                        Sbox(0, 5)
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ],
-          )
+            ),
+            SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                physics: const BouncingScrollPhysics(),
+                child: Column(
+                  children: List.generate(
+                    10,
+                    (index) => Container(
+                      width: double.infinity,
+                      height: 90,
+                      margin: const EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          color: const Color(0xFFF6F6F6),
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            width: 1,
+                            color: const Color(0xFFd1d1d1),
+                          )),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius:
+                                BorderRadius.circular(15), // Image border
+                            child: SizedBox.fromSize(
+                              size: Size.fromRadius(45),
+                              child: Image.network(
+                                //프로필 사진
+                                controller.myprofile.image,
+                              ),
+                            ),
+                          ),
+                          Sbox(20, 0),
+                          Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  controller.myprofile.nick,
+                                  style: const TextStyle(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Sbox(0, 15),
+                                Container(
+                                  width: 220,
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Row(
+                                        children: [
+                                          Image.asset(
+                                            //랭크 사진
+                                            "images/point.png",
+                                            width: 20,
+                                            height: 20,
+                                          ),
+                                          Text('  플레티넘'),
+                                        ],
+                                      ),
+                                      Row(
+                                        children: [
+                                          Text('300 PT  '),
+                                          Image.asset(
+                                            "images/point.png",
+                                            width: 20,
+                                            height: 20,
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ]),
+                        ],
+                      ),
+                    ),
+                  ),
+                ))
+          ]),
         ),
       ),
     );
