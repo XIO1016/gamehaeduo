4fe570ae449d7a43bb7dcf56e9c530fabd5e6571
thddh <dhthdrud@naver.com>
2022-11-23 17:18:41 +0900

    feat: update searchDuoController api

--- lib/controller/searchDuo/seachDuoController.dart
+++ lib/controller/searchDuo/seachDuoController.dart
@@ -23,7 +23,7 @@ class searchDuoController extends GetxController {
   getDuo() async {
     var getduo = await http.get(
         Uri.parse(urlBase +
-            'api/player/profiles/column?userIdx=$userId&size=10&page=${page.value}'),
+            'api/duo/search?userIdx=$userId&size=10&page=${page.value}'),
         headers: <String, String>{
           "content-type": "application/json",
           "accept": "application/json",
@@ -38,37 +38,26 @@ class searchDuoController extends GetxController {
       log('duoNum' + duoNum.value.toString());
       for (int i = 0; i < homePartnerDTO.length; i++) {
         Map j = homePartnerDTO[i];
+
         Duo a = Duo(
-            name: j['playerId'].toString(),
+            name: j['nickname'].toString(),
             rank: j['tier'],
-            position: ['탑', '정글'],
-            image:
-                'https://mblogthumb-phinf.pstatic.net/MjAxOTEwMjdfMzIg/MDAxNTcyMTQzMDk1MzIy.vmE4PCbENLckM1qDsOc0gXKV2R2uyVAIPIsLT-YayKEg.YTzw2hKln4nijfDEp3Y5Y22h5kLsBWkexI9UU6d5XMIg.PNG.yunam69/%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5.png?type=w800',
+            position: [],
+            image: j['profilePhotoUrl'],
             star: j['rating'],
-            playStyle: '',
-            introduce: '',
+            playStyle: j['playStyle'],
+            introduce: j['introduction'],
             price: j['price']);
+
+        if (j['top'] == 1) a.position.add('탑');
+        if (j['jungle'] == 1) a.position.add('정글');
+        if (j['mid'] == 1) a.position.add('미드');
+        if (j['ad'] == 1) a.position.add('원딜');
+        if (j['supporter'] == 1) a.position.add('서포터');
         duoList.add(a);
         log(duoList[i].name.toString());
       }
       page.value += 1;
     }
-  }
-
-  updateDuo() {
-    for (int i = 10; i < 20; i++) {
-      Duo a = Duo(
-          name: i.toString(),
-          rank: '플레티넘',
-          position: ['탑', '정글'],
-          image:
-              'https://mblogthumb-phinf.pstatic.net/MjAxOTEwMjdfMzIg/MDAxNTcyMTQzMDk1MzIy.vmE4PCbENLckM1qDsOc0gXKV2R2uyVAIPIsLT-YayKEg.YTzw2hKln4nijfDEp3Y5Y22h5kLsBWkexI9UU6d5XMIg.PNG.yunam69/%EC%8A%A4%ED%8F%B0%EC%A7%80%EB%B0%A5.png?type=w800',
-          star: 4.5,
-          playStyle: '즐겁게',
-          introduce: '안녕하세요',
-          price: 100);
-      duoList.add(a);
-    }
-    duoNum(20);
   }
 }
